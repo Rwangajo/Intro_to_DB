006fa2955b8967cc6b8349bdc5fd3754eeb59c41
Jonathan RWANGABWOBA <rwangajo@gmail.com>
2025-06-28 16:56:36 -0300

    Update task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- Script to insert a single row into the 'customer' table.
+-- The database name (e.g., alx_book_store) will be passed as an argument to the mysql command.
+
+INSERT INTO customer (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) VALUES
+(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
